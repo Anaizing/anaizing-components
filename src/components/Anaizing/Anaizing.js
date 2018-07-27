d3a92164ebccee8fbd9aaf470b3d03d9efa4a703
Anai Araya <anai.yusary@gmail.com>
2018-07-27 18:25:59 +1000

    playing with css

--- src/components/Anaizing/Anaizing.js
+++ src/components/Anaizing/Anaizing.js
@@ -1,15 +1,18 @@
 import React from 'react'
-import PropTypes from 'prop-types';
+import PropTypes from 'prop-types'
 
+/** A pretty lame component displaying the Author's name with a custom message */
 function Anaizing({message}) {
-    return <div>Anaizing.Design {message}</div>
+    return <div>By Anaizing {message}</div>
 }
 
 Anaizing.propTypes = {
-    /**
-     * Message to display
-     */
+    /** Message to display */
     message: PropTypes.string
 }
+
+Anaizing.defaultProps= {
+    message: 'Building Dreams'
+} 
 
 export default Anaizing
---
+++ src/docs/examples/Anaizing/ExampleAnaizning.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import Anaizing from '../../../components/Anaizing/Anaizing'
+
+/** Custom message */
+export default function ExampleAnaizing(){
+    return <Anaizing message="-Where there's a will there's a way" />
+}
