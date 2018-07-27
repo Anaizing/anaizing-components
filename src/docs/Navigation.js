4d3d077199e1419cd1319732e2e14c0d2a3801b3
Anai Araya <anai.yusary@gmail.com>
2018-07-27 12:02:59 +1000

    add notes to the navigation file

--- src/docs/Navigation.js
+++ src/docs/Navigation.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+//* Here we have an unordered list thats going to display a list of our components
+//* we'll map over the array of components passed in, and display an anchor for each one
+//* by convention the name of the component will be whats used in the url, so we place a # 
+//* in the url followed by the name of the component
 const Navigation = ({ components }) => {
     return (
         <ul className="navigation">
