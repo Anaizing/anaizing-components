fa42ecc94dc95d88c6689c6925e1af035dbc66bd
Anai Araya <anai.yusary@gmail.com>
2018-07-27 11:48:01 +1000

    add explanatory notes to docs.js

--- src/docs/Docs.js
+++ src/docs/Docs.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import Navigation from './Navigation';
 import ComponentPage from './ComponentPage';
-import componentData from '../../config/componentData';
+import componentData from '../../config/componentData'; //* reference to the component data file that we're generating every time we start the app or save
 
+//* Instead of pulling in a routing library we are holding the first segment of the url in STATE and
+//* using hash based urls to keep it simple. Each time the url changes we're going to update state...
 export default class Docs extends React.Component {
     constructor(props) {
         super(props);
@@ -10,13 +12,15 @@ export default class Docs extends React.Component {
             route: window.location.hash.substr(1)
         };
     }
-
+//* ...which you can see here where we add a hashchange event listener 
     componentDidMount() {
         window.addEventListener('hashchange', () => {
             this.setState({ route: window.location.hash.substr(1) })
         })
     }
-
+//* here in render we are using the route stored in state to get our hands on the corresponding component, by 
+//* convention the route and the url should match the components name, this will keep our routing very simple
+//* if there is no route specified we'll just display the first component in the list
     render() {
         const { route } = this.state;
         const component = route ? componentData.filter(component => component.name === route)[0] : componentData[0];
